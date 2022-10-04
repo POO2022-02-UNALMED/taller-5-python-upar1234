393de967ef6a36a78bb1bab8156e92e49186ab88
upar1234 <upar1234@gmail.com>
2022-10-04 10:23:09 -0500

    a

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -7,28 +7,20 @@ class Zoologico:
             self._zonas = list()
         else:
             self._zonas = zonas
-
     def getNombre(self):
         return self._nombre
-
     def setNombre(self, nombre):
         self._nombre = nombre
-
     def getUbicacion(self):
         return self._ubicacion
-
     def setUbicacion(self, ubicacion):
         self._ubicacion = ubicacion
-
     def getZona(self):
         return self._zonas
-
     def setZonas(self, zonas):
         self._zonas = zonas
-
     def agregarZonas(self, zona):
         self._zonas.append(zona)
-
     def cantidadTotalAnimales(self):
         animales = [i.cantidadAnimales() for i in self._zonas]
         return sum(animales)
