880e9cef85b3add1a326e38522276e35e3136b3c
upar1234 <upar1234@gmail.com>
2022-10-04 10:21:04 -0500

    s

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -12,31 +12,22 @@ class Animal:
 
     def getNombre(self):
         return self._nombre
-
     def setNombre(self, nombre):
         self._nombre = nombre
-
     def getEdad(self):
         return self._edad
-
     def setEdad(self, edad):
         self._edad = edad
-
     def getHabitat(self):
         return self._habitat
-
     def setHabitat(self, habitat):
         self._habitat = habitat
-
     def getGenero(self):
         return self._genero
-
     def setGenero(self, genero):
         self._genero = genero
-
     def getZona(self):
         return self._zona
-
     def setZona(self, zona):
         self._zona = zona
 
@@ -55,6 +46,5 @@ class Animal:
             return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el zoo {self._zona.getZoo()}"
         else:
             return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
-
     def movimiento(self):
         return "desplazarse"
